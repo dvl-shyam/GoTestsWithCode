066fd57941a6e8a850eb9e85dd472cd6dd0bfd7c
shyam kuntal <72796245+shyamkuntal@users.noreply.github.com>
2024-11-07 04:29:11 +0530

    hello world complete

--- HelloWorld/hello.go
+++ HelloWorld/hello.go
@@ -2,10 +2,51 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return "Hello, " + name
+// const englishHelloPrefix = "Hello, "
+
+// func Hello(name string) string {
+// 	if name == "" {
+// 		name = "World"
+// 	}
+// 	return englishHelloPrefix + name
+// }
+
+// func Hello(name string, language string) string {
+// 	if name == "" {
+// 		name = "World"
+// 	}
+// 	return englishHelloPrefix + name
+// }
+
+const (
+	spanish = "Spanish"
+	french  = "French"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", "french"))
 }
